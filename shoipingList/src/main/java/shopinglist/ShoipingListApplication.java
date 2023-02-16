756946df4c6419bff238fd9bd2856bd0c598ed1f
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-16 20:08:24 +0200

    shopping list app initial commit

---
+++ shoipingList/src/main/java/shopinglist/ShoipingListApplication.java
@@ -0,0 +1,13 @@
+package shopinglist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoipingListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoipingListApplication.class, args);
+    }
+
+}
---
+++ shoipingList/src/test/java/shopinglist/ShoipingListApplicationTests.java
@@ -0,0 +1,13 @@
+package shopinglist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoipingListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
