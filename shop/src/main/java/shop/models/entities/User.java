6824f274066a3d0773659473143c1c39c2637485
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-17 16:55:54 +0200

    create user entity

---
+++ shop/src/main/java/shop/models/entities/User.java
@@ -0,0 +1,24 @@
+package shop.models.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity{
+    @Column(nullable = false,unique = true)
+    private String username;
+    @Column(nullable = false,unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+}
