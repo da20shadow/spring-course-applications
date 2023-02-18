cb832edbe087968d2d91f457989c8a978821e0f1
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-18 19:55:16 +0200

    Created constant messages

---
+++ reseller-app/src/main/java/exam2023/constants/Messages.java
@@ -0,0 +1,33 @@
+package exam2023.constants;
+
+public interface Messages {
+
+    //USER
+    String USER_CREATED = "Successfully registered!";
+    String USER_UPDATED = "Successfully updated!";
+    String USER_DELETED = "Successfully deleted user!";
+
+    String USERNAME_TAKEN = "Username already taken";
+    String USERNAME_REQUIRED = "Username can not be empty!";
+    String USERNAME_INVALID_LENGTH = "Username must be between 3 and 20 characters long!";
+    String USER_NOT_FOUND = "User not found!";
+    String EMAIL_TAKEN = "Email already taken";
+    String EMAIL_INVALID = "Invalid email format!";
+    String EMAIL_REQUIRED = "Email is required!";
+    String PASSWORD_REQUIRED = "Password can not be empty!";
+    String PASSWORD_MISMATCH = "Password mismatch!";
+    String MIN_PASS_LENGTH_ERR = "Password must be at least 3 and max 20 characters long!";
+
+    String INVALID_CREDENTIALS = "Invalid username or password!";
+    String UNAUTHORIZED = "Unauthorized access!";
+    String ACCESS_DENIED = "Access denied!";
+
+    String INVALID_CONDITION = "Invalid condition!";
+
+    //OFFER
+    String OFFER_CREATED = "Successfully registered new OFFER!";
+    String OFFER_UPDATED = "Successfully updated OFFER!";
+    String OFFER_DELETED = "Successfully deleted OFFER!";
+
+    String OFFER_NOT_FOUND = "OFFER not found!";
+}
