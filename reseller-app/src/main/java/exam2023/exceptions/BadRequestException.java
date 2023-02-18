9fcf15615202d8490135ab56833bd65f04a69a4a
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-18 19:57:56 +0200

    added custom exception

---
+++ reseller-app/src/main/java/exam2023/exceptions/BadRequestException.java
@@ -0,0 +1,10 @@
+package exam2023.exceptions;
+
+public class BadRequestException extends RuntimeException{
+    public BadRequestException(String message) {
+        super(message);
+    }
+    public BadRequestException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
