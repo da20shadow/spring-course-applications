8e390497bfc3b8821a4244b4a1cf7b94098e095a
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-18 20:00:02 +0200

    Created logged user helper

---
+++ reseller-app/src/main/java/exam2023/utils/LoggedUser.java
@@ -0,0 +1,22 @@
+package exam2023.utils;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+@Getter
+@Setter
+@Component
+@SessionScope
+public class LoggedUser {
+    private Long id;
+    private String username;
+    public void clear(){
+        setId(null);
+        setUsername(null);
+    }
+    public boolean isLogged() {
+        return this.username != null && this.id != null;
+    }
+}
