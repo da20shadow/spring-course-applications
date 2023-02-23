879e5980c211fce48f840c80d6b7baf69fa75aa4
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-02-23 07:47:03 +0200

    Added entities

---
+++ rest-api/src/main/java/restapi/model/entities/AuthorEntity.java
@@ -0,0 +1,23 @@
+package restapi.model.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "authors")
+public class AuthorEntity extends BaseEntity {
+    private String name;
+    @OneToMany
+    private List<BookEntity> books;
+}
---
+++ rest-api/src/main/java/restapi/model/entities/BookEntity.java
@@ -0,0 +1,22 @@
+package restapi.model.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "books")
+public class BookEntity extends BaseEntity {
+    private String title;
+    private String isbn;
+    @ManyToOne
+    private AuthorEntity author;
+}
