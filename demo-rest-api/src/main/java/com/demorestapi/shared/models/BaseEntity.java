3223144157043840d6c2a384acd5b657f3024a18
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-03-01 17:45:01 +0200

    create base entity

---
+++ demo-rest-api/src/main/java/com/demorestapi/shared/models/BaseEntity.java
@@ -0,0 +1,21 @@
+package com.demorestapi.shared.models;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@MappedSuperclass
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
