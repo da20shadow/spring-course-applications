302631ac3d567d5efd3a4affde2d7925d9e7f31d
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-03-02 14:34:35 +0200

    added validations and application properties

--- demo-rest-api/src/main/java/com/demorestapi/goal/models/dtos/AddGoalDTO.java
+++ demo-rest-api/src/main/java/com/demorestapi/goal/models/dtos/AddGoalDTO.java
@@ -1,5 +1,6 @@
 package com.demorestapi.goal.models.dtos;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,7 +12,11 @@ import lombok.Setter;
 @AllArgsConstructor
 public class AddGoalDTO {
 
+    @NotEmpty(message = "Title can not be empty! Please, enter Goal Title")
     private String title;
+    @NotEmpty(message = "Description can not be empty! Please, enter Goal Description")
     private String description;
+    @NotEmpty(message = "Due date can not be empty! Please, enter Goal Due date")
     private String dueDate;
+
 }
