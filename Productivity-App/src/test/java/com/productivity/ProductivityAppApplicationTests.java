fec4bb9cca71899b86f2aa8c2163328883331ef3
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-03-15 13:50:31 +0200

    Initial commit

---
+++ Productivity-App/src/test/java/com/productivity/ProductivityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.productivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductivityAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
