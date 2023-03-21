edfbb2b675195ffd00e83ace7479d624d23086cb
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-03-21 17:44:12 +0200

    create target messages

---
+++ Spring-Security-Demo/src/main/java/com/security/targets/constants/TargetMessages.java
@@ -0,0 +1,18 @@
+package com.security.targets.constants;
+
+public class TargetMessages {
+
+    public static class ErrorMessages {
+        public static final String ADD_ERROR = "Unable to add new target!";
+        public static final String UPDATED_ERROR = "Unable to update target!";
+        public static final String DELETED_ERROR = "Unable to delete target!";
+        public static final String NOT_FOUND = "Target not found!";
+        public static final String NO_CHANGES_ERROR = "No changes!";
+    }
+
+    public static class SuccessMessages {
+        public static final String ADD_SUCCESS = "Successfully added new target!";
+        public static final String UPDATED_SUCCESS = "Successfully updated target!";
+        public static final String DELETED_SUCCESS = "Successfully deleted target!";
+    }
+}
