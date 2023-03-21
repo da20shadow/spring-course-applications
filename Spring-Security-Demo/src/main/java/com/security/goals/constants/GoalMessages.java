f4808c4a0fb74bf43559ba38026681acf64a9252
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-03-21 14:41:20 +0200

    goal messages error and success

---
+++ Spring-Security-Demo/src/main/java/com/security/goals/constants/GoalMessages.java
@@ -0,0 +1,18 @@
+package com.security.goals.constants;
+
+public class GoalMessages {
+
+    public static class ErrorGoalMessages {
+        public static final String NOT_FOUND = "Goal Not Found!";
+        public static final String ADD_ERROR = "Unable to added new goal!";
+        public static final String UPDATED_ERROR = "Unable to updated goal!";
+        public static final String DELETED_ERROR = "Unable to deleted goal!";
+        public static final String DUPLICATE_TITLE_ERROR = "A goal with this title already exists!";
+    }
+
+    public static class SuccessGoalMessages {
+        public static final String ADD_SUCCESS = "Successfully added new goal!";
+        public static final String UPDATED_SUCCESS = "Successfully updated goal!";
+        public static final String DELETED_SUCCESS = "Successfully deleted goal!";
+    }
+}
