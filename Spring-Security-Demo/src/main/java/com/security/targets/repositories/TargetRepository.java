7db9c1515fa0a14e26121b74a247ebfd27c0c29d
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-03-22 21:07:22 +0200

    get total targets

--- Spring-Security-Demo/src/main/java/com/security/targets/repositories/TargetRepository.java
+++ Spring-Security-Demo/src/main/java/com/security/targets/repositories/TargetRepository.java
@@ -2,6 +2,8 @@ package com.security.targets.repositories;
 
 import com.security.targets.models.entities.Target;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -12,4 +14,7 @@ public interface TargetRepository extends JpaRepository<Target,Long> {
     Optional<Target> findByIdAndUserId(Long targetId, Long userId);
 
     Set<Target> findAllByGoalIdAndUserId(Long goalId, Long userId);
+
+    @Query("SELECT COUNT(t) FROM Target t WHERE t.goal.id = :goalId")
+    int countTargetsByGoalId(Long goalId);
 }
