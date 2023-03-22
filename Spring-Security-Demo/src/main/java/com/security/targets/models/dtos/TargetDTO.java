90469767e13e1233ebc8e53f86c334a49e3892b5
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-03-22 21:06:31 +0200

    add id and created at date

--- Spring-Security-Demo/src/main/java/com/security/targets/models/dtos/TargetDTO.java
+++ Spring-Security-Demo/src/main/java/com/security/targets/models/dtos/TargetDTO.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.Builder;
 
+import java.time.LocalDateTime;
 import java.util.Set;
 
 @Getter
@@ -15,8 +16,10 @@ import java.util.Set;
 @NoArgsConstructor
 @AllArgsConstructor
 public class TargetDTO {
+    private Long id;
     private String title;
     private String description;
+    private LocalDateTime createdAt;
     private int totalTasks;
     private int totalCompletedTasks;
     private Set<Task> tasks;
