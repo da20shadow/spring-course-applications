0f3ac7abc36e1aad3032a302b43a77c65be5b884
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-03-23 19:32:26 +0200

    task exceptions added

---
+++ Spring-Security-Demo/src/main/java/com/security/tasks/exceptions/TaskException.java
@@ -0,0 +1,7 @@
+package com.security.tasks.exceptions;
+
+public class TaskException extends RuntimeException {
+    public TaskException(String message) {
+        super(message);
+    }
+}
---
+++ Spring-Security-Demo/src/main/java/com/security/tasks/exceptions/TaskNotFoundException.java
@@ -0,0 +1,7 @@
+package com.security.tasks.exceptions;
+
+public class TaskNotFoundException extends TaskException {
+    public TaskNotFoundException(String message) {
+        super(message);
+    }
+}
