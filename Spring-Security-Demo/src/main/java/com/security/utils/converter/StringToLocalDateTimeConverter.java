f08c96a887f13114765981acf3178fe214430b95
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-03-24 22:00:29 +0200

    added new string to localdatetime converter

---
+++ Spring-Security-Demo/src/main/java/com/security/utils/converter/StringToLocalDateTimeConverter.java
@@ -0,0 +1,31 @@
+package com.security.utils.converter;
+
+import org.modelmapper.Converter;
+import org.modelmapper.spi.MappingContext;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Component
+public class StringToLocalDateTimeConverter implements Converter<String, LocalDateTime> {
+
+    @Override
+    public LocalDateTime convert(MappingContext<String, LocalDateTime> context) {
+        String source = context.getSource();
+        if (source == null || source.trim().isEmpty()) {
+            return null;
+        }
+
+        DateTimeFormatter formatter;
+        if (source.length() <= 10) {
+            formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        } else {
+            formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        }
+
+        return LocalDateTime.parse(source, formatter);
+    }
+}
+
+
