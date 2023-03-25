c2e6886a0b9722671965f3b316cedda9232c0562
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-03-25 21:33:38 +0200

    update add task dto

--- Spring-Security-Demo/src/main/java/com/security/tasks/models/dtos/AddTaskDTO.java
+++ Spring-Security-Demo/src/main/java/com/security/tasks/models/dtos/AddTaskDTO.java
@@ -17,21 +17,13 @@ public class AddTaskDTO {
     @NotBlank(message = "Title cannot be blank")
     @Size(min = 2, max = 145, message = "Title must be between 2 and 145 characters")
     private String title;
-
     private String description;
-
     private String status;
-
     private String priority;
-
     private boolean urgent = false;
-
     private boolean important = false;
-
-    private Long targetId;
-
+    private Long targetId = null;
     private String startDate;
-
     private String dueDate;
 
 }
