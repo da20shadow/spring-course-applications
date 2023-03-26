1f0fa18a8709cf8019c136be05bc8d6091479d3b
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-03-26 21:15:33 +0300

    edit checklist dto

---
+++ Spring-Security-Demo/src/main/java/com/security/tasks/models/dtos/EditChecklistItemDTO.java
@@ -0,0 +1,15 @@
+package com.security.tasks.models.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EditChecklistItemDTO {
+    private String title;
+    private boolean completed;
+}
