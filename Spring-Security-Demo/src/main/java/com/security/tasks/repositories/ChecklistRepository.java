40faa333e273e3aa1f621cceac816aebf564e500
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-03-26 21:16:45 +0300

    checklist repo

--- Spring-Security-Demo/src/main/java/com/security/tasks/repositories/ChecklistRepository.java
+++ Spring-Security-Demo/src/main/java/com/security/tasks/repositories/ChecklistRepository.java
@@ -1,12 +1,17 @@
 package com.security.tasks.repositories;
 
-import com.security.tasks.models.entities.CheckTask;
+import com.security.tasks.models.entities.ChecklistItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
-public interface ChecklistRepository extends JpaRepository<CheckTask,Long> {
-    List<CheckTask> findAllByTaskIdAndUserId(Long taskId, Long userId);
+public interface ChecklistRepository extends JpaRepository<ChecklistItem,Long> {
+    List<ChecklistItem> findAllByTaskIdAndUserId(Long taskId, Long userId);
+
+    Optional<ChecklistItem> findByTitleAndTaskIdAndUserId(String title, Long taskId, Long userId);
+
+    Optional<ChecklistItem> findByIdAndUserId(Long itemId, Long userId);
 }
